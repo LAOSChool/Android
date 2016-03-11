cd698a1a83e29bc3c726eba433d1c0edc71a5170
NguyenHue <mutang1967@gmail.com>
2016-03-11 15:10:04 +0700

    add test data in ScreenListTeacher

--- app/src/main/java/com/laoschool/screen/ScreenListTeacher.java
+++ app/src/main/java/com/laoschool/screen/ScreenListTeacher.java
@@ -1,8 +1,11 @@
 package com.laoschool.screen;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,7 +13,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.laoschool.R;
+import com.laoschool.adapter.RecylerViewScreenListTeacherAdapter;
+import com.laoschool.adapter.RecylerViewScreenSettingAdapter;
 import com.laoschool.view.FragmentLifecycle;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,16 +26,42 @@ import com.laoschool.view.FragmentLifecycle;
 public class ScreenListTeacher extends Fragment implements FragmentLifecycle {
 
 
+    private String seletedTearcher;
+
     public ScreenListTeacher() {
         // Required empty public constructor
     }
 
+    public void setSeletedTearcher(String seletedTearcher) {
+        this.seletedTearcher = seletedTearcher;
+    }
+
+    public String getSeletedTearcher() {
+        return seletedTearcher;
+    }
+
+    public interface IScreenListTeacher {
+        void gotoScreenTeacherDetailsformScreenListTeacher(String teacherSelected);
+    }
+
+    public IScreenListTeacher iScreenListTeacher;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.screen_list_teacher, container, false);
+        View view = inflater.inflate(R.layout.screen_list_teacher, container, false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.mRecylerViewScreenListTeacher);
+
+        //init adapter
+        final List<String> strings = Arrays.asList(getResources().getStringArray(R.array.listTeacher));
+        RecylerViewScreenListTeacherAdapter adapter = new RecylerViewScreenListTeacherAdapter(this, strings);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 1);
+
+        //set adapter
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
     @Override
@@ -39,7 +73,7 @@ public class ScreenListTeacher extends Fragment implements FragmentLifecycle {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         //set display menu item
-        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_screen_list_teacher, menu);
     }
 
     @Override
@@ -54,5 +88,12 @@ public class ScreenListTeacher extends Fragment implements FragmentLifecycle {
 
     public static Fragment instantiate(int containerId, String currentRole) {
         return new ScreenListTeacher();
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        iScreenListTeacher = (IScreenListTeacher) activity;
     }
 }
