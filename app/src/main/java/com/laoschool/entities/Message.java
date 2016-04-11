7623b23c5bb2400015a5306fea2643988a96fc5e
NguyenHue <mutang1967@gmail.com>
2016-04-11 16:22:43 +0700

    add colum name DB message

--- app/src/main/java/com/laoschool/entities/Message.java
+++ app/src/main/java/com/laoschool/entities/Message.java
@@ -1,9 +1,8 @@
 package com.laoschool.entities;
 
-import com.google.gson.Gson;
+import android.provider.BaseColumns;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.Gson;
 
 /**
  * Created by Tran An on 23/03/2016.
@@ -50,30 +49,35 @@ public class Message {
 
     String messageType;
 
+    String cc_list;
+
+    int cId;
 
     public Message() {
     }
 
-    public Message(int channel, int class_id, String content, String from_user_name, int from_usr_id, int id, int imp_flg, int is_read, int is_sent, String messageType, int msg_type_id, String other, String read_dt, int school_id, String schoolName, String sent_dt, String title, String to_user_name, int to_usr_id) {
-        this.channel = channel;
-        this.class_id = class_id;
-        this.content = content;
-        this.from_user_name = from_user_name;
-        this.from_usr_id = from_usr_id;
+    public Message(int id, int school_id, int class_id, int from_usr_id, String from_user_name, int to_usr_id, String to_user_name, String content, int channel, int is_sent, String sent_dt, int is_read, String read_dt, String other, int msg_type_id, int imp_flg, String title, String schoolName, String messageType, String cc_list, int aId) {
         this.id = id;
-        this.imp_flg = imp_flg;
-        this.is_read = is_read;
-        this.is_sent = is_sent;
-        this.messageType = messageType;
-        this.msg_type_id = msg_type_id;
-        this.other = other;
-        this.read_dt = read_dt;
         this.school_id = school_id;
-        this.schoolName = schoolName;
-        this.sent_dt = sent_dt;
-        this.title = title;
-        this.to_user_name = to_user_name;
+        this.class_id = class_id;
+        this.from_usr_id = from_usr_id;
+        this.from_user_name = from_user_name;
         this.to_usr_id = to_usr_id;
+        this.to_user_name = to_user_name;
+        this.content = content;
+        this.channel = channel;
+        this.is_sent = is_sent;
+        this.sent_dt = sent_dt;
+        this.is_read = is_read;
+        this.read_dt = read_dt;
+        this.other = other;
+        this.msg_type_id = msg_type_id;
+        this.imp_flg = imp_flg;
+        this.title = title;
+        this.schoolName = schoolName;
+        this.messageType = messageType;
+        this.cc_list = cc_list;
+        this.cId = aId;
     }
 
     public int getId() {
@@ -228,6 +232,22 @@ public class Message {
         this.to_user_name = to_user_name;
     }
 
+    public String getCc_list() {
+        return cc_list;
+    }
+
+    public void setCc_list(String cc_list) {
+        this.cc_list = cc_list;
+    }
+
+    public int getClienId() {
+        return cId;
+    }
+
+    public void setClienId(int cId) {
+        this.cId = cId;
+    }
+
     public String toJson() {
         Gson gson = new Gson();
         String jsonString = gson.toJson(this);
@@ -238,5 +258,102 @@ public class Message {
         Gson gson = new Gson();
         Message message = gson.fromJson(jsonString, Message.class);
         return message;
+    }
+
+
+    /* Inner class that defines the table contents */
+    public static abstract class MessageColumns implements BaseColumns {
+
+        public static final String TABLE_NAME = "messages";
+
+        public static final String COLUMN_NAME_ID = "id";
+        public static final String COLUMN_NAME_SCHOOL_ID= "school_id";
+        public static final String COLUMN_NAME_CLASS_ID= "class_id";
+
+        public static final String COLUMN_NAME_FROM_USR_ID = "from_usr_id";
+        public static final String COLUMN_NAME_FROM_USER_NAME = "from_user_name";
+
+        public static final String COLUMN_NAME_TO_USR_ID = "to_usr_id";
+        public static final String COLUMN_NAME_TO_USER_NAME = "to_user_name";
+
+        public static final String COLUMN_NAME_CONTENT = "content";
+        public static final String COLUMN_NAME_MSG_TYPE_ID= "msg_type_id";
+        public static final String COLUMN_NAME_CHANNEL = "channel";
+
+        public static final String COLUMN_NAME_IS_SENT = "is_sent";
+        public static final String COLUMN_NAME_IS_READ = "is_read";
+
+        public static final String COLUMN_NAME_SENT_DT = "sent_dt";
+        public static final String COLUMN_NAME_READ_DT = "read_dt";
+
+        public static final String COLUMN_NAME_IMP_FLG = "imp_flg";
+
+        public static final String COLUMN_NAME_OTHER = "other";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_CC_LIST = "cc_list";
+        public static final String COLUMN_NAME_SCHOOL_NAME = "schoolName";
+        public static final String COLUMN_NAME_MESSAGE_TYPE = "messageType";
+        public static final String COLUMN_NAME_CLIENT_ID = "cId";
+
+        public static final int COLUMN_NAME_ID_INDEX_0 = 0;
+        public static final int COLUMN_NAME_SCHOOL_ID_INDEX_1 = 1;
+        public static final int COLUMN_NAME_CLASS_ID_INDEX_2= 2;
+
+        public static final int COLUMN_NAME_FROM_USR_ID_INDEX_3 =3;
+        public static final int COLUMN_NAME_FROM_USER_NAME_INDEX_4 =4;
+
+        public static final int COLUMN_NAME_TO_USR_ID_INDEX_5 = 5;
+        public static final int COLUMN_NAME_TO_USER_NAME_INDEX_6 = 6;
+
+        public static final int COLUMN_NAME_CONTENT_INDEX_7 = 7;
+        public static final int COLUMN_NAME_MSG_TYPE_ID_INDEX_8= 8;
+        public static final int COLUMN_NAME_CHANNEL_INDEX_9 = 9;
+
+        public static final int COLUMN_NAME_IS_SENT_INDEX_10 = 10;
+        public static final int COLUMN_NAME_IS_READ_INDEX_11 = 11;
+
+        public static final int COLUMN_NAME_SENT_DT_INDEX_12 = 12;
+        public static final int COLUMN_NAME_READ_DT_INDEX_13 = 13;
+
+        public static final int COLUMN_NAME_IMP_FLG_INDEX_14 = 14;
+
+        public static final int COLUMN_NAME_OTHER_INDEX_15 = 15;
+        public static final int COLUMN_NAME_TITLE_INDEX_16 = 16;
+        public static final int COLUMN_NAME_CC_LIST_INDEX_17 = 17;
+        public static final int COLUMN_NAME_SCHOOL_NAME_INDEX_18 = 18;
+        public static final int COLUMN_NAME_MESSAGE_TYPE_INDEX_19 = 19;
+        public static final int COLUMN_NAME_CLIENT_ID_INDEX_20 = 20;
+
+
+        public static final String CREATE_MESSAGES_TABLE =  "CREATE TABLE " + TABLE_NAME + "("
+                + COLUMN_NAME_ID + " INTEGER," //0
+                + COLUMN_NAME_TITLE + " TEXT,"//1
+                + COLUMN_NAME_SCHOOL_ID + " INTEGER,"//2
+                + COLUMN_NAME_CLASS_ID + " INTEGER,"//3
+
+                + COLUMN_NAME_FROM_USR_ID + " INTEGER,"//4
+                + COLUMN_NAME_FROM_USER_NAME + " TEXT,"//5
+
+                + COLUMN_NAME_TO_USR_ID + " INTEGER,"//6
+                + COLUMN_NAME_TO_USER_NAME + " TEXT,"//7
+
+                + COLUMN_NAME_CONTENT + " TEXT,"//8
+                + COLUMN_NAME_MSG_TYPE_ID + " INTEGER,"//9
+
+                + COLUMN_NAME_CHANNEL + " INTEGER,"//10
+
+                + COLUMN_NAME_IS_SENT + " INTEGER,"//11
+                + COLUMN_NAME_SENT_DT + " TEXT,"//12
+                + COLUMN_NAME_IS_READ + " INTEGER,"//13
+
+                + COLUMN_NAME_READ_DT + " TEXT,"//14
+                + COLUMN_NAME_IMP_FLG + " INTEGER,"//15
+
+                + COLUMN_NAME_OTHER + " TEXT,"//16
+                + COLUMN_NAME_CC_LIST + " TEXT,"//17
+                + COLUMN_NAME_SCHOOL_NAME + " TEXT,"//18
+                + COLUMN_NAME_MESSAGE_TYPE + " TEXT,"//19
+                + COLUMN_NAME_CLIENT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT" //20
+                + ")";
     }
 }
