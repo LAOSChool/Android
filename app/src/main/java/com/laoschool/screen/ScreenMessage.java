d7686a20caa9f2356b3499729bf907529d1c2cde
NguyenHue <mutang1967@gmail.com>
2016-05-06 08:55:15 +0700

    test lifecycle message

--- app/src/main/java/com/laoschool/screen/ScreenMessage.java
+++ app/src/main/java/com/laoschool/screen/ScreenMessage.java
@@ -7,6 +7,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -117,6 +118,7 @@ public class ScreenMessage extends Fragment implements FragmentLifecycle {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView()");
         return _defineScreenMessage(inflater, container);
     }
 
@@ -258,6 +260,7 @@ public class ScreenMessage extends Fragment implements FragmentLifecycle {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
         if (getArguments() != null) {
@@ -290,6 +293,7 @@ public class ScreenMessage extends Fragment implements FragmentLifecycle {
 
     @Override
     public void onAttach(Activity activity) {
+        Log.d(TAG,"onAttach(Activity)");
         super.onAttach(activity);
         iScreenMessage = (IScreenMessage) activity;
         if (LaoSchoolShared.myProfile == null) {
@@ -590,6 +594,7 @@ public class ScreenMessage extends Fragment implements FragmentLifecycle {
 
     @Override
     public void onResume() {
+        Log.d(TAG, "onResume()");
         super.onResume();
         if (LaoSchoolShared.myProfile == null) {
             HomeActivity homeActivity = (HomeActivity) getActivity();
@@ -597,4 +602,45 @@ public class ScreenMessage extends Fragment implements FragmentLifecycle {
         }
     }
 
+    @Override
+    public void onAttach(Context context) {
+        Log.d(TAG,"onAttach(Context)");
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onActivityCreated()");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(TAG, "onStart()");
+        super.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(TAG, "onPause()");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(TAG, "onStop()");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach()");
+        super.onDetach();
+    }
 }
