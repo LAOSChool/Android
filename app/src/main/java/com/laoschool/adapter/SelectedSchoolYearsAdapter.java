af4a18a0463f5c9d782713a315232cc94666bab1
NguyenHue <mutang1967@gmail.com>
2016-06-15 16:18:06 +0700

    fix adapter selected year

--- app/src/main/java/com/laoschool/adapter/SelectedSchoolYearsAdapter.java
+++ app/src/main/java/com/laoschool/adapter/SelectedSchoolYearsAdapter.java
@@ -1,46 +1,57 @@
 package com.laoschool.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.laoschool.R;
 import com.laoschool.entities.SchoolYears;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Hue on 6/15/2016.
  */
 public class SelectedSchoolYearsAdapter extends ArrayAdapter {
-
-
     private final List<SchoolYears> schoolYearsList;
 
-    public SelectedSchoolYearsAdapter(Context context, int resource, List<SchoolYears> schoolYearsList) {
-        super(context, resource, schoolYearsList);
-        this.schoolYearsList = schoolYearsList;
+    public SelectedSchoolYearsAdapter(Context context, List<SchoolYears> objects) {
+        super(context, R.layout.row_selected_school_year, objects);
+        this.schoolYearsList = objects;
     }
 
-    public TextView getView(int position, View convertView, ViewGroup parent) {
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        SchoolYears schoolYears = schoolYearsList.get(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_selected_school_year, parent, false);
+        }
+        // Lookup view for data population
+        TextView tvName = (TextView) convertView.findViewById(R.id.lbSchoolYears);
 
-        TextView v = (TextView) super
-                .getView(position, convertView, parent);
-        v.setText(schoolYearsList.get(position).getYears());
-        v.setId(schoolYearsList.get(position).getId());
-        return v;
+        // Populate the data into the template view using the data object
+        tvName.setText(schoolYears.getYears());
+        // Return the completed view to render on screen
+        return convertView;
     }
 
-    public TextView getDropDownView(int position, View convertView,
-                                    ViewGroup parent) {
-        TextView v = (TextView) super
-                .getView(position, convertView, parent);
-        v.setPadding(30, 30, 30, 30);
-        v.setText(schoolYearsList.get(position).getYears());
-        v.setId(schoolYearsList.get(position).getId());
-        return v;
-    }
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        SchoolYears schoolYears = schoolYearsList.get(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_selected_school_year_list, parent, false);
+        }
+        // Lookup view for data population
+        TextView tvName = (TextView) convertView.findViewById(R.id.lbSchoolYears);
 
+        // Populate the data into the template view using the data object
+        tvName.setText(schoolYears.getYears());
+        // Return the completed view to render on screen
+        return convertView;
+    }
 }
 
